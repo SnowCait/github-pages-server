181e79faf061a430cfede9bd6f41d8dede6113d3
SnowCait <SnowCait@users.noreply.github.com>
2022-03-27 00:18:19 +0900

    WIP

---
+++ bin/github-pages.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const express = require('express');
+const path = require('path');
+
+const hostname = 'localhost';
+const port = 8000;
+const repository = path.basename(__dirname);
+const folder = '/docs';
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/${repository}/`);
+});
+
+app.use(`/${repository}`, express.static(path.join(__dirname, folder)));
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, `${folder}/404.html`));
+});
